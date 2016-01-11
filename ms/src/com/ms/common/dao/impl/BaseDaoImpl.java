853da145e6e650ec70ba1a62249fbb4525350c44
biantd <biantaidong@126.com>
2016-01-11 22:31:37 +0800

    优化部分base类和rbac类

--- ms/src/com/ms/common/dao/impl/BaseDaoImpl.java
+++ ms/src/com/ms/common/dao/impl/BaseDaoImpl.java
@@ -1,6 +1,7 @@
 package com.ms.common.dao.impl;
 
 import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
 import java.util.List;
 import java.util.Map;
 
@@ -9,9 +10,18 @@ import org.hibernate.SessionFactory;
 import com.ms.common.Pager;
 import com.ms.common.dao.BaseDao;
 
-public class BaseDaoImpl<T> implements BaseDao<T>{
+public class BaseDaoImpl<T> implements BaseDao<T> {
+	// sessionFactory
 	private SessionFactory sessionFactory;
-	
+	// 反射类对象
+	private Class<T> clazz;
+
+	public BaseDaoImpl() {
+		ParameterizedType pt = (ParameterizedType) getClass()
+				.getGenericSuperclass();
+		this.clazz = (Class<T>) pt.getActualTypeArguments()[0];
+	}
+
 	public void setSessionFactory(SessionFactory sessionFactory) {
 		this.sessionFactory = sessionFactory;
 	}
@@ -23,12 +33,12 @@ public class BaseDaoImpl<T> implements BaseDao<T>{
 
 	@Override
 	public void deleteById(Serializable id) {
-		
+
 	}
 
 	@Override
 	public void update(T t) {
-		
+
 	}
 
 	@Override
@@ -46,6 +56,12 @@ public class BaseDaoImpl<T> implements BaseDao<T>{
 	public Pager getPageByCondition(Map<String, String> map) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public List<T> getAll() {
+		return sessionFactory.getCurrentSession()
+				.createQuery("from "+clazz.getName()).list();
 	}
 
 }
--- ms/src/com/ms/common/entity/BaseEntity.java
+++ ms/src/com/ms/common/entity/BaseEntity.java
@@ -1,44 +1,67 @@
 package com.ms.common.entity;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
+import org.hibernate.annotations.GenericGenerator;
+@MappedSuperclass
 public class BaseEntity implements Serializable{
 
 	/**
-	 * 
+	 * 实体类的父类
 	 */
 	private static final long serialVersionUID = -4295426802999880307L;
+	// id 采用uuid生成方法没有意义
+	private String  id;
 	//创建时间
-	private String createTime;
+	private Date createTime;
 	//最后修改时间
-	private String modifyTime;
+	private Date modifyTime;
 	//创建人
-	private String createId;
+	private String createUser;
 	public BaseEntity() {
 		super();
 	}
 	
+	@Id
 	@Column(length = 32)
-	public String getCreateTime() {
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid") 
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	@Temporal(TemporalType.TIMESTAMP) 
+	public Date getCreateTime() {
 		return createTime;
 	}
-	public void setCreateTime(String createTime) {
+	public void setCreateTime(Date createTime) {
 		this.createTime = createTime;
 	}
-	@Column(length = 32)
-	public String getModifyTime() {
+	@Temporal(TemporalType.TIMESTAMP)
+	public Date getModifyTime() {
 		return modifyTime;
 	}
-	public void setModifyTime(String modifyTime) {
+	public void setModifyTime(Date modifyTime) {
 		this.modifyTime = modifyTime;
 	}
 	@Column(length = 32)
-	public String getCreateId() {
-		return createId;
+	public String getCreateUser() {
+		return createUser;
 	}
-	public void setCreateId(String createId) {
-		this.createId = createId;
+	
+	public void setCreateUser(String createUser) {
+		this.createUser = createUser;
 	}
 }
