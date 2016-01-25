c71d12f850779e10f38a8a069ea024274a603d43
biantd <biantaidong@126.com>
2016-01-25 22:24:42 +0800

    部分代码优化

--- ms/src/com/ms/rbac/dao/impl/GroupDaoImpl.java
+++ ms/src/com/ms/rbac/dao/impl/GroupDaoImpl.java
@@ -1,11 +1,14 @@
 package com.ms.rbac.dao.impl;
 
+import org.springframework.stereotype.Repository;
+
 import com.ms.common.dao.impl.BaseDaoImpl;
 import com.ms.rbac.dao.GroupDao;
 import com.ms.rbac.dao.UserDao;
 import com.ms.rbac.entity.Group;
 import com.ms.rbac.entity.User;
 
+@Repository
 public class GroupDaoImpl extends BaseDaoImpl<Group> implements GroupDao {
 
 }
--- ms/src/com/ms/rbac/dao/impl/UserDaoImpl.java
+++ ms/src/com/ms/rbac/dao/impl/UserDaoImpl.java
@@ -1,9 +1,12 @@
 package com.ms.rbac.dao.impl;
 
+import org.springframework.stereotype.Repository;
+
 import com.ms.common.dao.impl.BaseDaoImpl;
 import com.ms.rbac.dao.UserDao;
 import com.ms.rbac.entity.User;
 
+@Repository
 public class UserDaoImpl extends BaseDaoImpl<User> implements UserDao {
 
 	@Override
