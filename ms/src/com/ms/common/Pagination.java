caf239d12ee33cb2ccd43a1868013f709c608db8
bian_td <biantaidong@126.com>
2016-07-20 22:42:13 +0800

    page

---
+++ ms/src/com/ms/common/Pagination.java
@@ -0,0 +1,20 @@
+package com.ms.common;
+
+import java.util.List;
+
+public class Pagination<T> {
+
+	public Pagination(int pageIndex, int pageSize, int intValue) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Pagination(int pageIndex, int pageSize, long rowsCount) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setItems(List items) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
