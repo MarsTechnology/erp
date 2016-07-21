f6a97fbf40446a2b7d3fe01d73b1879845c2ab2e
bian_td <biantaidong@126.com>
2016-07-21 20:38:01 +0800

    优化

--- ms/src/com/ms/rbac/entity/User.java
+++ ms/src/com/ms/rbac/entity/User.java
@@ -12,93 +12,45 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import com.ms.common.entity.BaseEntity;
 
 @Entity
 @Table(name = "RBAC_USER")
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class User extends BaseEntity {
 
 	private static final long serialVersionUID = 2698479255878048424L;
-	// ��¼���
-	private String logName;
-	// ����
-	private String password;
-	// ����
-	private String email;
-	// ״̬��
-	/*
-	 * @Column(length = 32) private String lock;
+	/**
+	 * 登录名称
 	 */
-	// Ա��
-	// private Emp emp;
-	// ����
+	@Column(length = 50, nullable = false)
+	private String logName;
+	/**
+	 * 登录密码
+	 */
+	@Column(length = 30, nullable = false)
+	private String password;
+	/**
+	 * 电子邮箱
+	 */
+	@Column(length = 50)
+	private String email;
+	/**
+	 * 用户部门
+	 */
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "groupId", nullable = false)
 	private Group group;
-	// �û���ɫ
+	/**
+	 * 用户角色
+	 */
+	@OneToMany(mappedBy = "user", cascade = { CascadeType.ALL }, fetch = FetchType.LAZY)
 	private List<UserRole> userRoles;
 
 	public User() {
-	}
-
-	@Column(length = 50, nullable = false)
-	public String getLogName() {
-		return logName;
-	}
-
-	public void setLogName(String logName) {
-		this.logName = logName;
-	}
-
-	@Column(length = 30, nullable = false)
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	@Column(length = 50)
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	/*
-	 * public String getLock() { return lock; }
-	 * 
-	 * public void setLock(String lock) { this.lock = lock; }
-	 */
-	// @OneToOne(fetch = FetchType.EAGER) //����Ĭ��ֵ
-	// @JoinColumn(name = "empId", nullable = false)
-	// public Emp getEmp() {
-	// return emp;
-	// }
-	//
-	// public void setEmp(Emp emp) {
-	// this.emp = emp;
-	// }
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "groupId", nullable = false)
-	public Group getGroup() {
-		return group;
-	}
-
-	public void setGroup(Group group) {
-		this.group = group;
-	}
-
-	@OneToMany(mappedBy = "user", cascade = { CascadeType.ALL }, fetch = FetchType.LAZY)
-	public List<UserRole> getUserRoles() {
-		return userRoles;
-	}
-
-	public void setUserRoles(List<UserRole> userRoles) {
-		this.userRoles = userRoles;
 	}
 
 }
