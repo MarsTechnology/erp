a5b833a95acc27abaed33eed4ac7d0164e8a644f
bian_td <biantaidong@126.com>
2016-07-21 20:28:40 +0800

    优化

--- ms/src/com/ms/rbac/dao/GroupDao.java
+++ ms/src/com/ms/rbac/dao/GroupDao.java
@@ -1,11 +1,7 @@
 package com.ms.rbac.dao;
 
-import org.springframework.stereotype.Repository;
-
 import com.ms.common.dao.BaseDao;
 import com.ms.rbac.entity.Group;
-import com.ms.rbac.entity.User;
-
 
 public interface GroupDao extends BaseDao<Group> {
 
