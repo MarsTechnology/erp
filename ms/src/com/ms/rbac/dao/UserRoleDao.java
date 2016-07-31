7d662c668d17e6b21f7cc66431e23cc740f7700c
bian_td <biantaidong@126.com>
2016-07-31 16:22:27 +0800

    userrole

---
+++ ms/src/com/ms/rbac/dao/UserRoleDao.java
@@ -0,0 +1,12 @@
+package com.ms.rbac.dao;
+
+import com.ms.common.dao.BaseDao;
+import com.ms.rbac.entity.UserRole;
+
+/**
+ * @author biantd 2016年7月24日
+ * @describe
+ */
+public interface UserRoleDao extends BaseDao<UserRole> {
+
+}
---
+++ ms/src/com/ms/rbac/dao/impl/UserRoleDaoImpl.java
@@ -0,0 +1,17 @@
+package com.ms.rbac.dao.impl;
+
+import org.springframework.stereotype.Repository;
+
+import com.ms.common.dao.impl.BaseDaoImpl;
+import com.ms.rbac.dao.UserRoleDao;
+import com.ms.rbac.entity.UserRole;
+
+/**
+ * @author biantd 2016年7月24日
+ * @describe
+ */
+@Repository
+public class UserRoleDaoImpl extends BaseDaoImpl<UserRole> implements
+		UserRoleDao {
+
+}
